e6dd2990ba16f983f3c9ee146d313482a1d43062
haditariq <hadi-tariq@live.co.uk>
2023-06-25 15:42:09 +0200

    stories: container

--- stories/Container.stories.ts
+++ stories/Container.stories.ts
@@ -13,7 +13,8 @@ type Story = StoryObj<typeof Container>;
 
 export const Primary: Story = {
   args: {
-    className: 'flex',
+    className: 'flex bg-primary p-10',
+    children: "A JSX element or an list can be passed here!"
   },
 };
 
